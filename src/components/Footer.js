1d733101387af16a8ff47bcb1ac72952678a90ed
Karolina <karmieczkowska@gmail.com>
2023-06-02 14:23:36 +0200

    Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import FacebookIcon from "@mui/icons-material/Facebook";
+// import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import '../styles/Footer.css'
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className="footer">
       <div className="icons">
-        <FacebookIcon />
-        <LinkedInIcon />
-        <GitHubIcon />
+        {/* <FacebookIcon /> */}
+        <NavLink to={"https://www.linkedin.com/in/karolina-mieczkowska/"} style={{color: "black"}}><LinkedInIcon /></NavLink>
+        <NavLink to={"https://github.com/karmieczkowska"} style={{color: "black"}}><GitHubIcon /></NavLink>
       </div>
       <p>&copy; {new Date().getFullYear()} Karolina Mieczkowska</p>
     </div>
