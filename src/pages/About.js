2b374f8a33691cb621f3df3d711f11053942fb12
Karolina <karmieczkowska@gmail.com>
2023-06-02 18:59:05 +0200

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import HeroSection2 from '../components/HeroSection2';
+import "../styles/About.css"
+
+import MyPhoto from "../assets/mojeImg.png"
 
 const About = () => {
   return (
@@ -8,6 +11,15 @@ const About = () => {
         heading="About me"
         text="React Front-End developer with passion for learning and ceating"
       />
+      <div className="about-me">
+        <div><img src={MyPhoto} alt="me" /></div>
+        <p>
+        Organized, communicative and quick-to-learn adept at front end programming
+        with a passion for both personal growth and for software development. Seeking
+        an opportunity to apply my passion for coding to a talented engineering team to
+        develop quality solutions. Team player with an eye for detail.
+        </p>
+      </div>
     </div>
   );
 };
