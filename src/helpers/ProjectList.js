d42bccde1aaa4c6da9c0e45382b8d7c0e6529d01
Karolina <karmieczkowska@gmail.com>
2023-06-19 15:58:38 +0200

    added balance app

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,11 +1,13 @@
 import llaPhoto from '../assets/lla-img.png'
 import calcPhoto from '../assets/calc-img.png'
 import clockPhoto from '../assets/clock-img.png'
+import balancePhoto from '../assets/balance-img.png'
 import todoPhoto from '../assets/todo-list.png'
 import memoPhoto from '../assets/memo-img.png'
 
 import llaVideo from '../assets/lla-video.mp4'
 import calcVideo from '../assets/calculator-video.mp4'
+import balanceVideo from '../assets/balance-video.mp4'
 import clockVideo from '../assets/clock-video.mp4'
 import todoVideo from '../assets/todo-list-video.mp4'
 import memoVideo from '../assets/memo-video.mp4'
@@ -28,12 +30,12 @@ export const ProjectList = [
         demo: "https://karmieczkowska.github.io/Calculator-JS/"
     },
     {
-        name: "Clock JS",
-        image: clockPhoto,
-        skills: "JavaScript, HTML, CSS",
-        link: "https://github.com/karmieczkowska/clock-js",
-        video: clockVideo,
-        demo: "https://karmieczkowska.github.io/clock-js/"
+        name: "Balance App",
+        image: balancePhoto,
+        skills: "TypeScript, HTML, CSS",
+        link: "https://github.com/karmieczkowska/balance-app",
+        video: balanceVideo,
+        demo: "https://karmieczkowska.github.io/balance-app/?"
     },
     {
         name: "To do list",
@@ -47,8 +49,16 @@ export const ProjectList = [
         name: " Memo game",
         image: memoPhoto,
         skills: "JavaScript, HTML, CSS",
-        video: "https://github.com/karmieczkowska/memo-game",
+        link: "https://github.com/karmieczkowska/memo-game",
         video: memoVideo,
         demo: "https://karmieczkowska.github.io/memo-game/"
+    },
+    {
+        name: "Clock JS",
+        image: clockPhoto,
+        skills: "JavaScript, HTML, CSS",
+        link: "https://github.com/karmieczkowska/clock-js",
+        video: clockVideo,
+        demo: "https://karmieczkowska.github.io/clock-js/"
     },
 ]
